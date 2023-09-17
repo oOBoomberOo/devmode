e0eef83da74e96aa36d89ee816bcd3e39b691843
oOBoomberOo <boomberisalreadytaken@gmail.com>
2023-09-17 17:36:32 +0900

    Fixed ResourceEventFactory's file matcher.
    This changed is necessary because the path given by WatchEvent is not a real path, so Files.isRegularFile could not find the expected file and always false.

--- src/main/java/me/boomber/devmode/filewatcher/ResourceEventFactory.java
+++ src/main/java/me/boomber/devmode/filewatcher/ResourceEventFactory.java
@@ -2,27 +2,28 @@ package me.boomber.devmode.filewatcher;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.WatchEvent;
+import java.nio.file.*;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class ResourceEventFactory {
+    private final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.mcfunction");
+
     @Nullable ResourceEvent create(WatchEvent<Path> event) {
+
         var kind = event.kind();
         var path = event.context();
 
         // this event also triggered when directory's children gets created or deleted
-        if (kind.equals(ENTRY_MODIFY) && Files.isRegularFile(path)) {
+        if (kind == ENTRY_MODIFY && matcher.matches(path)) {
             return new ModifyEvent(path);
         }
 
-        if (kind.equals(ENTRY_CREATE)) {
+        if (kind == ENTRY_CREATE) {
             return new CreateEvent(path);
         }
 
-        if (kind.equals(ENTRY_DELETE)) {
+        if (kind == ENTRY_DELETE) {
             return new DeleteEvent(path);
         }
 
